cb8ca3ed6230489e144e2481f51a80f9415041aa
Bruno Lucho Macedo <bmacedo@weg.net>
2022-01-07 12:46:54 -0300

    Function carouselImg created.

--- src/index.js
+++ src/index.js
@@ -2,44 +2,55 @@ import './assets/index.css'
 import axios from 'axios'
 
 var photos = []
-var i = 0
+var index = 0
+var Url = null
 
 function getPhotos() {
   axios.get('https://jsonplaceholder.typicode.com/photos')
-    .then(function (response) {
+    .then((response) => {
       photos = response.data.slice(0, 5)
       console.log(photos)
     })
-    .catch(function (error) {
-      console.log(error)
+    .catch((error) => {
+      alert(error)
     })
 }
 
-getPhotos()
-
-//Next Photo
-function increment() {
-  i = +i
-  console.log(i)
-}
-
-//Previous photo
-document.getElementById("prev").addEventListener("click", decrement);
-function decrement() {
-  if (i > 0){
-    i = --i
-    console.log(i)
-  }else {
-    i = 5
-    console.log(i)
-  }
-}
-
-const carouselImg = () => {
+function carouselImg () {
   var img = document.createElement('img')
-  img.src = 'https://via.placeholder.com/600/771796'
+  Url = photos[index]
+  console.log(Url)
+  img.src = "https://via.placeholder.com/600/92c952" //photos[index].url
   img.className = 'carousel-img'
   return img
 }
-
 document.getElementById('carousel').appendChild(carouselImg())
+
+/*
+window.onload = () => {
+  getPhotos() 
+}
+*/
+//Next Photo
+document.getElementById('next').addEventListener('click', increment)
+function increment() {
+  if ( index < 4) {
+    index = index + 1
+    console.log(index)
+  } else {
+    index = 0
+    console.log(index)
+  }
+}
+
+//Previous photo
+document.getElementById("prev").addEventListener('click', decrement);
+function decrement() {
+  if (index > 0){
+    index = --index
+    carouselImg()
+  } else {
+    index = 4
+    carouselImg()
+  }
+}
